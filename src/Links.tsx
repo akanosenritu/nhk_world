1c1fa1afc4bd33a50b31f449e6eabddd70b0392d
akasen@lenovo-x250 <akasen.org@gmail.com>
2021-11-12 13:11:25 +0900

    update

--- src/Links.tsx
+++ src/Links.tsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom"
 
 const Links = () => {
   const today = sub(new Date(), {hours: 4})
-  const week = [0, 1, 2, 3, 4, 5, 6].map(num => sub(today, {days: num})).map(date => format(date, "yyyy-MM-dd"))
+  const week = [0, 1, 2, 3, 4, 5, 6].map(num => sub(today, {days: num})).map(date => format(date, "MM/dd"))
   return <Box sx={{display: "flex"}}>
-    This week: {week.map(date => <Box mx={1}><Link to={date}>{date}</Link></Box>)}
+    {week.map(date => <Box ml={1}><Link to={date}>{date}</Link></Box>)}
   </Box>
 }
 
--- src/Main.tsx
+++ src/Main.tsx
@@ -2,18 +2,17 @@ import React from "react"
 import {format, sub} from "date-fns"
 import Day from "./Day"
 import Links from "./Links"
-import {Container} from "@mui/material"
 
 const Main: React.FC = () => {
   const today = format(
     sub(new Date(), {hours: 4}), 
     "yyyy-MM-dd"
     )
-  return <Container maxWidth="md">
+  return <div>
     <h3>Los articulos de días de NHK World Spanish</h3>
     <Links />
     <Day date={today}/>
-  </Container>
+  </div>
 }
 
 export default Main
